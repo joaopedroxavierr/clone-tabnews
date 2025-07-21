939a3a613891395def0b5580a652c9f3bcbe2a13
João Pedro Xavier <115355590+joaopedroxavierr@users.noreply.github.com>
2025-07-21 22:17:41 +0000

    Adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home () {
+    return <p>Eu amo o seu sorriso. Amo a forma como você enxerga o mundo. Amo as suas manias. Te amo assim, meu amor: por completo.</p>
+}
+
+export default Home;
